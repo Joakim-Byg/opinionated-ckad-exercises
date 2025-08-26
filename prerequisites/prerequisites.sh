eadbcbaf36ee2c7030fe11491fda884647a02c5f
Joakim Byg <jby@netic.dk>
2025-08-27 00:38:22 +0200

    Added kustomize to the installations

--- prerequisites/prerequisites.sh
+++ prerequisites/prerequisites.sh
@@ -9,8 +9,8 @@ sudo mv kubectl /usr/bin
 # installing helm
 curl -lO https://get.helm.sh/helm-v3.18.6-linux-386.tar.gz
 tar -zxvf helm-v3.18.6-linux-386.tar.gz
-chmod +x linux-amd64/helm
-sudo mv linux-amd64/helm /usr/bin/helm
+chmod +x linux-386/helm
+sudo mv linux-386/helm /usr/bin/helm
 
 # installing k9s
 curl -LO https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_Linux_amd64.tar.gz
@@ -22,3 +22,9 @@ sudo mv k9s /usr/bin
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/bin/kind
+
+# installing kustomize
+curl -lO https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.7.1/kustomize_v5.7.1_linux_amd64.tar.gz
+tar xf kustomize_v5.7.1_linux_amd64.tar.gz
+chmod +x kustomize
+sudo mv kustomize /usr/bin/kustomize
